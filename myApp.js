43ec1f07e5926e392093e9029aca2af7f30e9a3c
kylestech95 <kylestech95@outlook.com>
2024-01-12 09:03:17 -0500

    feat: post-middleware

--- myApp.js
+++ myApp.js
@@ -5,6 +5,13 @@ let app = express();
 
 
 
+// middleware
+app.use((req, res, next)=>{
+    console.log(`${req.method} ${req.path} - ${req.ip}`);
+    next();
+  })
+
+
 // console.log => terminal
 console.log("Hello World")
 
@@ -15,8 +22,10 @@ app.get("/", function (req, res) {
     res.sendFile(__dirname + "/views/index.html")
 })
 
+
 // app.use 
 app.use("/public", express.static(__dirname + "/public"))
+
 
 // .env testing
 app.get("/json",function(req,res){
@@ -29,6 +38,11 @@ app.get("/json",function(req,res){
     }
     res.json({"message": result})
 })
+
+
+
+
+  
 
 
 
